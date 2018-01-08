5fd15cc2a67acf9a3e6d6bae9224f369eabc480b
Pawel <pawel.html@gmail.com>
2018-01-08 15:31:39 +0100

    foreach in knockoutJS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 var ViewModel = function(){
-
+    // model:
+    this.clickCount = ko.observable(0);
+    this.name = ko.computed(function(){
+        return this.clickCount() < 10 ? "Newborn" : this.clickCount() < 20 ? "Infand" : "Big Cat";
+    }, this);
+    this.nickNames = ko.observableArray(["mały", "drzewiec"]);
+    this.imgSrc = ko.observable('img/1.jpg');
+    this.incrementCounter = function(){
+        this.clickCount(this.clickCount()+1);
+    }
 }
 
 ko.applyBindings(new ViewModel());
